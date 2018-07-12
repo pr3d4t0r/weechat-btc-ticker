6245632c797c2d46c104bc520e914245a5a156dd
Eugene Ciurana <eugenex@cime.net>
2018-07-11 21:16:29 -0700

    Removed builtins, standardlibrary -

--- btc_ticker.py
+++ btc_ticker.py
@@ -40,9 +40,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
-from builtins import *
-from future import standard_library
-standard_library.install_aliases()
 
 from datetime import datetime
 
